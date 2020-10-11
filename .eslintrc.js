05282e4e3d5844f02bd633136d8990b2f68431ad
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-11 06:29:17 -0300

    configs for linter, babel and gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+const RULES = {
+  OFF: "off",
+  WARN: "warn",
+  ERROR: "error"
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": RULES.OFF,
+    "react/react-in-jsx-scope": RULES.OFF,
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: [2, "always"],
+    "no-undef": RULES.OFF
+  }
+};
