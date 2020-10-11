f6dd711bbd06d484b4ad6f283b864c32861b4797
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-11 06:35:28 -0300

    added new colors

---
+++ styles/theme.js
@@ -0,0 +1,17 @@
+import { theme } from "@chakra-ui/core";
+
+const breakpoints = ["300px", "768px", "1024px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+
+export const CustomTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    ecommerce: {
+      100: "#318fb5",
+    },
+  },
+  breakpoints,
+};
