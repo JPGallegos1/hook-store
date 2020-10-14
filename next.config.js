8dcbe2d72498838fe71d9c23de9d3c1ef2334093
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-14 00:42:29 -0300

    add: new config for next.config and environment variables

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// This is the way how's work the environment variables' flow in Nextjs:
+// .env.local -> next.config.js -> client bundle
+// Also, I implemented it as constants too.
+// More info: https://medium.com/frontend-digest/environment-variables-in-next-js-9a272f0bf655
+
+module.exports = {
+  env: {
+    API: process.env.NEXT_PUBLIC_BASE_URL,
+    PRODUCTS_ENDPOINT: process.env.NEXT_PUBLIC_PRODUCTS_ENDPOINT
+  }
+};
